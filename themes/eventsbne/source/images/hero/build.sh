e8b20c44073f85e3244dc713da1bdaba07f6cb5b
Ash Kyd <ash@kyd.com.au>
2018-01-21 22:55:08 +1000

    fix broken 1366 webp images

--- themes/eventsbne/source/images/hero/build.sh
+++ themes/eventsbne/source/images/hero/build.sh
@@ -5,6 +5,6 @@ convert source/southbank-beach.png -quality 30 southbank-beach.webp
 convert source/southbank-beach-1920.png -quality 60 southbank-beach-1920.jpg
 convert source/southbank-beach-1920.png -quality 30 southbank-beach-1920.webp
 convert source/southbank-beach-1366.png -quality 60 southbank-beach-1366.jpg
-convert source/southbank-beach-1366.png -quality 60 southbank-beach-1366.jpg
+convert source/southbank-beach-1366.png -quality 30 southbank-beach-1366.webp
 convert source/southbank-beach-600.png -quality 60 southbank-beach-600.jpg
 convert source/southbank-beach-600.png -quality 30 southbank-beach-600.webp
